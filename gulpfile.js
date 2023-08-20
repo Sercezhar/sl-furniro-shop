fb222e275846704546c21f395cc1e526770346a5
Oleksandr Havryliuk <deolis.ua@gmail.com>
2023-08-20 13:14:59 +0300

    update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,22 @@
-"use strict";
+'use strict';
 
-const { src, dest } = require("gulp");
-const gulp = require("gulp");
-const autoprefixer = require("gulp-autoprefixer");
-const cssbeautify = require("gulp-cssbeautify");
-const removeComments = require("gulp-strip-css-comments");
-const rename = require("gulp-rename");
-const rigger = require("gulp-rigger");
-const sass = require("gulp-sass")(require("sass"));
-const cssnano = require("gulp-cssnano");
-const uglify = require("gulp-uglify");
-const notify = require("gulp-notify");
-const plumber = require("gulp-plumber");
-const imagemin = require("gulp-imagemin");
-const del = require("del");
-const fileinclude = require("gulp-file-include");
-const browserSync = require("browser-sync").create();
+const { src, dest } = require('gulp');
+const gulp = require('gulp');
+const autoprefixer = require('gulp-autoprefixer');
+const changed = require('gulp-changed');
+const cssbeautify = require('gulp-cssbeautify');
+const cssnano = require('gulp-cssnano');
+const fileinclude = require('gulp-file-include');
+const htmlmin = require('gulp-htmlmin');
+const imagemin = require('gulp-imagemin');
+const notify = require('gulp-notify');
+const plumber = require('gulp-plumber');
+const removeComments = require('gulp-strip-css-comments');
+const rename = require('gulp-rename');
+const sass = require('gulp-sass')(require('sass'));
+const del = require('del');
+const webpack = require('webpack-stream');
+const browserSync = require('browser-sync').create();
 
 /* ========== [PATHS] ========== */
 const buildFolder = `./dist`;
@@ -53,7 +54,7 @@ function server() {
     server: {
       baseDir: buildFolder,
     },
-    logLevel: "info",
+    logLevel: 'info',
     cors: true,
     notify: false,
     port: 3000,
@@ -62,26 +63,29 @@ function server() {
 
 function html() {
   return src(path.src.html, { base: srcFolder })
+    .pipe(changed(path.build.html, { hasChanged: changed.compareContents }))
     .pipe(plumber())
     .pipe(fileinclude())
+    .pipe(htmlmin({ collapseWhitespace: true }))
     .pipe(dest(path.build.html))
     .pipe(browserSync.stream());
 }
 
 function scss() {
   return src(path.src.css, { base: `${srcFolder}/scss/` })
+    .pipe(changed(path.build.css))
     .pipe(
       plumber({
         errorHandler: function (err) {
           notify.onError({
-            title: "SCSS Error",
-            message: "Error: <%= error.message %>",
+            title: 'SCSS Error',
+            message: 'Error: <%= error.message %>',
           })(err);
-          this.emit("end");
+          this.emit('end');
         },
       })
     )
-    .pipe(sass({ outputStyle: "compressed" }))
+    .pipe(sass({ outputStyle: 'compressed' }))
     .pipe(autoprefixer())
     .pipe(cssbeautify())
     .pipe(dest(path.build.css))
@@ -96,8 +100,8 @@ function scss() {
     .pipe(removeComments())
     .pipe(
       rename({
-        suffix: ".min",
-        extname: ".css",
+        suffix: '.min',
+        extname: '.css',
       })
     )
     .pipe(dest(path.build.css))
@@ -106,33 +110,26 @@ function scss() {
 
 function js() {
   return src(path.src.js, { base: `${srcFolder}/js/` })
+    .pipe(changed(path.build.js))
     .pipe(
       plumber({
         errorHandler: function (err) {
           notify.onError({
-            title: "JavaScript Error",
-            message: "Error: <%= error.message %>",
+            title: 'JavaScript Error',
+            message: 'Error: <%= error.message %>',
           })(err);
-          this.emit("end");
+          this.emit('end');
         },
       })
     )
-    .pipe(fileinclude())
-    .pipe(rigger())
-    .pipe(dest(path.build.js))
-    .pipe(uglify())
-    .pipe(
-      rename({
-        suffix: ".min",
-        extname: ".js",
-      })
-    )
+    .pipe(webpack(require('./webpack.config.js')))
     .pipe(dest(path.build.js))
     .pipe(browserSync.stream());
 }
 
 function images() {
   return src(path.src.images, { base: `${srcFolder}/images/` })
+    .pipe(changed(path.build.images))
     .pipe(
       imagemin([
         imagemin.gifsicle({ interlaced: true }),
@@ -150,9 +147,10 @@ function images() {
 }
 
 function fonts() {
-  return src(path.src.fonts, { base: `${srcFolder}/fonts/` }).pipe(
-    browserSync.stream()
-  );
+  return src(path.src.fonts, { base: `${srcFolder}/fonts/` })
+    .pipe(changed(path.build.fonts))
+    .pipe(dest(path.build.fonts))
+    .pipe(browserSync.stream());
 }
 
 function clean() {
